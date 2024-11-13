924c2dfbd1a7b1d852a7d8676d1ae6e3ae87b020
SundayNT <azterisk13@gmail.com>
2024-11-13 11:07:43 +0700

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css'
+import NavBar from './components/NavBar'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-green-500">
-      Hello world!
-    </h1>
+    <NavBar />
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+
+
+export default function NavBar() {
+  return (
+    <div>
+      <ul>
+        <li>
+            
+        </li>
+      </ul>
+    </div>
+  )
+}
