c43d267ebb6dd3af2d386a117b1a62a723e8fbcd
SundayNT <azterisk13@gmail.com>
2024-11-19 17:35:20 +0700

    .

--- src/components/Graduated.jsx
+++ src/components/Graduated.jsx
@@ -5,7 +5,7 @@ export default function Graduated() {
     <div>
         <h1 className="text-2xl text-white font-semibold mb-8 text-center">Education</h1>
         <ul className="list-disc list-inside">
-            <li className="text-xl text-slate-200 mb-5">Junior Software Developer at Generation Thailand - 8</li>
+            <li className="text-xl text-slate-200 mb-5">Junior Software Developer at Generation Thailand (Cohort 8) </li>
             <li className="text-xl text-slate-200">Bachelor Degree of Science in Medical Technologist at Rangsit University</li>
         </ul>
     </div>
