80aad671b2b4f3a1a621ca115800d0a2a2dbd745
SundayNT <azterisk13@gmail.com>
2024-11-21 22:47:34 +0700

    edited graduated component by adding experience topic

--- src/components/Graduated.jsx
+++ src/components/Graduated.jsx
@@ -1,13 +1,43 @@
-
+import { useState } from 'react'
+import SundayCV from "/CV/SundayCV.pdf"
 
 export default function Graduated() {
+  const [switchTopic, setSwitchTopic] = useState('');
+
   return (
     <div>
-        <h1 className="text-2xl text-white font-semibold mb-8 text-center">Education</h1>
-        <ul className="list-disc list-inside">
-            <li className="text-xl text-slate-200 mb-5">Junior Software Developer at Generation Thailand (Cohort 8) </li>
-            <li className="text-xl text-slate-200">Bachelor Degree of Science in Medical Technologist at Rangsit University</li>
+        <ul className="flex justify-around">
+        <li className="text-2xl text-white font-semibold mb-8 text-center"><button onClick={() => setSwitchTopic('Experience')}>Experience</button></li>
+        <li className="text-2xl text-white font-semibold mb-8 text-center"><button onClick={() => setSwitchTopic('Education')}>Education</button></li>
         </ul>
+        {switchTopic === 'Experience' && <Experience />}
+        {switchTopic === 'Education' && <Education />}
     </div>
   )
 }
+
+export function Experience() {
+  return (
+    <div>
+      <div>
+      <ul className="list-disc list-inside mb-16">
+            <li className="text-xl text-slate-200 mb-5">Junior Software Developer (Cohort 8) at Generation Thailand Bootcamp <br /><br />
+            (September 2024 - December 2024)</li>
+            <li className="text-xl text-slate-200 mb-5">Medical technologist at FACULTY OF ALLIED HEALTH SCIENCES, 
+            CHULALONGKORN UNIVERSITY <br /><br />(2021 - 2022)</li>
+      </ul>
+      <a href={SundayCV} target='_blank' className='text-teal-400 hover:text-teal-500 active:text-teal-600  text-lg font-semibold underline'>Click for more details</a>
+      </div>
+    </div>
+  )
+}
+export function Education() {
+  return (
+    <div>
+      <ul className="list-disc list-inside">
+            <li className="text-xl text-slate-200">Bachelor Degree of Science in Medical Technologist at Rangsit University</li>
+      </ul>
+    </div>
+  )
+}
+
